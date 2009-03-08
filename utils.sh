0b3b7fe865a71d4d30db1956dbdc03263d617526
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-03-08 05:28:03 +0000

    create trunk, add utils.sh, svntag

---
+++ utils.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Time-stamp: <03/07/2009 21:56:34 Saturday by ahei>
+
+export PAGER='/usr/bin/most -s'
+export BROWSER='/usr/bin/most -s'
+export PS4='+$LINENO '
+export HISTSIZE=9999999
+export PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007\n"'
+
+bce()
+{
+	bc <<< "scale=3; $@"
+}
+
+alias smth='screen ssh ahei0802@bbs.newsmth.net'
+alias e='emacsclient -n'
+alias cp='cp -r'
+alias grep='grep --color'
+alias mysql='mysql --pager=more --prompt="\u@\h:\d>\_"'
+alias watch='watch -n 1 -d'
+alias cman='man -M /usr/share/man/zh_CN/'
+alias utol="tr '[A-Z]' '[a-z]'"
+alias trim='sed -r "s/^[[:space:]]*|[[:space:]]*$//g"'
+alias TRIM="trim | tr '[A-Z]' '[a-z]'"
+alias jip='java -javaagent:/usr/share/jip/profile/profile.jar -Dprofile.properties=/usr/share/jip/profile/profile.properties'
