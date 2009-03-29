bd9c19d2f07f023a572fbd4bbce14c28b75dd877
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-03-29 21:42:34 +0000

    add alias remoteall

--- temp/temp.sh
+++ temp/temp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Time-stamp: <03/28/2009 20:16:44 Saturday by ahei>
+# Time-stamp: <03/29/2009 21:40:42 星期日 by ahei>
 
 alias sahei='ssh 172.0.1.208 -lahei'
 alias s48='ssh 172.0.1.249 -lroot'
@@ -14,3 +14,5 @@ alias s72='ssh 192.168.158.172 -lroot'
 alias s76='ssh 192.168.158.176 -lroot'
 alias s77='ssh 192.168.158.177 -lroot'
 alias s80='ssh 192.168.158.180 -lroot'
+
+alias remoteall='remote.sh -lroot -f /etc/shell/temp/servers/all-servers.txt'
