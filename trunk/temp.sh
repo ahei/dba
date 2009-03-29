cc896feb6ba980f441c7a4565adb1e5d812aaf93
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-03-29 10:14:57 +0000

    add temp.sh

---
+++ trunk/temp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Time-stamp: <03/28/2009 20:16:44 Saturday by ahei>
+
+alias sahei='ssh 172.0.1.208 -lahei'
+alias s48='ssh 172.0.1.249 -lroot'
+alias s49='ssh 172.0.1.248 -lahei'
+alias s00='ssh 172.0.1.100 -lroot'
+alias s01='ssh 172.0.1.101 -lroot'
+alias s02='ssh 172.0.1.101 -lroot'
+
+alias s71='ssh 192.168.158.171 -lroot'
+alias s72='ssh 192.168.158.172 -lroot'
+alias s76='ssh 192.168.158.176 -lroot'
+alias s77='ssh 192.168.158.177 -lroot'
+alias s80='ssh 192.168.158.180 -lroot'
