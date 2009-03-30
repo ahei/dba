518fafe7abff35a5999185c162b7708bd8bb7a2f
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-03-30 13:24:25 +0000

    small change

--- trunk/temp/temp.sh
+++ trunk/temp/temp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Time-stamp: <03/30/2009 10:13:33 星期一 by ahei>
+# Time-stamp: <03/30/2009 13:23:48 星期一 by ahei>
 
 alias sahei='ssh 172.0.1.208 -lahei'
 alias s48='ssh 172.0.1.248 -lroot'
 alias s49='ssh 172.0.1.249 -lahei'
-alias s00='ssh 172.0.1.100 -lroot'
-alias s01='ssh 172.0.1.101 -lroot'
-alias s02='ssh 172.0.1.101 -lroot'
+alias s50='ssh 172.0.1.250 -lroot'
+alias s51='ssh 172.0.1.251 -lroot'
+alias s52='ssh 172.0.1.252 -lroot'
 
 alias s71='ssh 192.168.158.171 -lroot'
 alias s72='ssh 192.168.158.172 -lroot'
