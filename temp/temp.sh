3f43d7a5eac5119d7363ca1671a286c0b30e54ce
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-04-22 01:56:14 +0000

    small change

--- temp/temp.sh
+++ temp/temp.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-# Time-stamp: <03/30/2009 13:23:48 星期一 by ahei>
+# Time-stamp: <04/22/2009 09:56:12 星期三 by ahei>
 
 alias sahei='ssh 172.0.1.208 -lahei'
 alias s48='ssh 172.0.1.248 -lroot'
+alias s48a='ssh 172.0.1.248 -lahei'
 alias s49='ssh 172.0.1.249 -lahei'
+alias s49r='ssh 172.0.1.249 -lroot'
 alias s50='ssh 172.0.1.250 -lroot'
 alias s51='ssh 172.0.1.251 -lroot'
 alias s52='ssh 172.0.1.252 -lroot'
+alias s50a='ssh 172.0.1.250 -lahei'
+alias s51a='ssh 172.0.1.251 -lahei'
+alias s52a='ssh 172.0.1.252 -lahei'
 
 alias s71='ssh 192.168.158.171 -lroot'
 alias s72='ssh 192.168.158.172 -lroot'
 alias s76='ssh 192.168.158.176 -lroot'
 alias s77='ssh 192.168.158.177 -lroot'
 alias s80='ssh 192.168.158.180 -lroot'
+alias s71a='ssh 192.168.158.171 -lroot'
+alias s72a='ssh 192.168.158.172 -lahei'
+alias s76a='ssh 192.168.158.176 -lahei'
+alias s77a='ssh 192.168.158.177 -lahei'
+alias s80a='ssh 192.168.158.180 -lahei'
 
 alias remoteall='remote.sh -lroot -f /etc/shell/temp/servers/all-servers.txt'
