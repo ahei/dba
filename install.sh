8376be8dd6d6c364880d7d4d543bb2d14285b0d6
ahei <ahei@040aa0f8-db7a-41c5-7f85-d4e6c68ed24a>
2009-05-08 03:42:19 +0000

    Add temp hostname to hosts file.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# Time-stamp: <04/27/2009 11:27:03 星期一 by ahei>
+# Time-stamp: <05/08/2009 11:40:52 星期五 by ahei>
 
 readonly PROGRAM_NAME="install.sh"
 readonly PROGRAM_VERSION="1.0"
+
+HOSTS_FILE=/etc/hosts
 
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
@@ -36,12 +38,11 @@ EOF
     exit "$code"
 }
 
-install()
+writeToFile()
 {
-    src="$1"
+    line="$1"
     dst="$2"
 
-    line=". $src"
     if ! grep -qFx "${line}" "$dst"; then
         printf "\n$line" >> "$dst"
     fi
@@ -88,10 +89,15 @@ fi
 ln -sf "${bin}"/.mostrc ~
 ln -sf "${bin}"/.toprc ~
 
-install "$bin/utils.sh" "$profile"
-install "$bin/history.sh" "$profile"
-install "$bin/temp/temp.sh" "$profile"
+writeToFile ". $bin/utils.sh" "$profile"
+writeToFile ". $bin/history.sh" "$profile"
+writeToFile ". $bin/temp/temp.sh" "$profile"
 
+IFS=$'\n'
+for i in `cat "$bin"/temp/temp-hosts`; do
+    writeToFile "$i" "$HOSTS_FILE"
+done
+    
 cp "$bin"/common.sh "$installDir"
 
 "$bin"/svntag -i "$installDir"
